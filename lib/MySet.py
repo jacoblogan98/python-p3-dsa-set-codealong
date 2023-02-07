9dfed79e2dea888a66595a58c497d2ee11ff822a
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-02-07 16:19:41 -0500

    passed tests

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    def __init__(self, list=[]):
+        self.dictionary = {}
+        for value in list:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+        return self
+
+    def __str__(self):
+        printList = [str(key) for key in self.dictionary]
+        
+        return f"MySet: {{{','.join(printList)}}}"
